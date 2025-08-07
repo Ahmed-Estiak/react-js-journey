46c537c08dddaf010ef04d1442929efbcb171b4e
Ahmed-Estiak <ahmedestiak14@gmail.com>
2025-08-07 22:48:51 +0300

    new 0.1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +19,7 @@ function App() {
       <h1>Hello from React!</h1>
       <p>This is a React component.</p>
     </div>
+    
       </header>
     </div>
   );
